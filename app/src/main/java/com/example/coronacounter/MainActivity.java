fe6d5c9c72a4bc2eec7b51b1c36483717e26f275
guzamaguza <guzamaguza@gmail.com>
2020-04-19 13:56:03 -0700

    Making Progress... Still need to perfect counter and visibility of warning message.

--- app/src/main/java/com/example/coronacounter/MainActivity.java
+++ app/src/main/java/com/example/coronacounter/MainActivity.java
@@ -2,8 +2,6 @@ package com.example.coronacounter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -12,7 +10,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 //import android.support.v7.app.AppCompatActivity;
-import android.os.VibrationEffect;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -22,31 +19,41 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.lang.Math;
-import java.util.Timer;
 import java.util.TimerTask;
-import java.util.logging.Handler;
+import android.os.Handler;
+import java.util.logging.LogRecord;
 
 import android.os.Vibrator;
 
 
 
 public class MainActivity extends AppCompatActivity {
-    private Timer myTimer;
-    Timer myTimer = new Timer();
-    myTimer.scheduleAtFixedRate(new TimerTask() {
-            View myView = findViewById(R.id.searchButton);
-            myView.performClick();
-    }, 0, 30000);//every 30 seconds
+    int counter = 0;
+    
+    /*
+    public void timer( String[] args ){
+        Timer myTimer = new Timer();
+        myTimer.scheduleAtFixedRate(
+            new TimerTask() {
+            View myView = findViewById(R.id.broadcastReceiver);
+            myView.startDiscovery();
+        },0,30000);//every 30 seconds
+    }
+    */
 
+    Handler handler = new Handler();
+    private Runnable periodicUpdate = new Runnable () {
+        @Override
+        public void run() {
+            // scheduled another events to be in 10 seconds later
+            handler.postDelayed(periodicUpdate, 10*1000); //milliseconds
+                    // below is whatever you want to do
 
+        }
+    };
 
-    //TextView exposureTextView;
-    //TextView counterTextView;
-    //tools:ignore="numberEditText";
 
     TextView alertTextView;
-    //alertTextView.setBackgroundColor(Color.RED);
-    //alertTextView.setTextColor(Color.parseColor("#FFFFFF"));
     TextView violationTextView;
     TextView counterTextView;
 
@@ -85,8 +92,14 @@ public class MainActivity extends AppCompatActivity {
                 double base = 10;
                 double distance_m = Math.pow(base,exponent);
                 double distance_ft = distance_m * 3.28;
+                String distance = Double.toString(distance_ft);
+                String distance_round = distance.substring(0, (distance.length() - 12));
+                //double distance_ft_round = Math.round(distance_ft , 2);
+                //String distance_ft_round = Double.toString(distance_ft);
+                //String output_distance = String.format("%.2f", distance_ft_round);
+                //DecimalFormat formater = new DecimalFormat("0.00");
                 //double counter = 0;
-                int counter = 0;
+
 
                 if (distance_ft < 6.00) {
                     // Get instance of Vibrator from current Context
@@ -96,15 +109,42 @@ public class MainActivity extends AppCompatActivity {
                     TextView text = (TextView) findViewById(R.id.counterTextView);
                     counter = counter + 1;
                     text.setText(String.valueOf(counter));
-                    //alertTextView.setVisibility(View.VISIBLE);
+
+                    final TextView alert = findViewById(R.id.alertTextView);
+                    alert.setVisibility(View.VISIBLE);
+
+/*
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    alert.setVisibility(View.VISIBLE);
+                                }
+                            });
+                        }
+                    },5000);
+
+
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (!isDestroyed() && !isFinishing()) {
+                                alert.setVisibility(View.INVISIBLE);
+                            }
+                        }
+                    },5000);
+
+*/
                 }
 
                     if (!addresses.contains(address)) {
                     addresses.add(address);
                     String deviceString = "";
                     if (name == null || name.equals("")) {
-                        deviceString = address + " - RSSI " + rssi + "dBm" + distance_ft;
-
+                        //deviceString = address + " - RSSI " + rssi + "dBm" + distance_ft;
+                        deviceString = "Device:   " + address + "              " + "Distance: " + distance_round + "  Feet Away!!!";
                         /*
                         if (distance_ft < 6.00) {
                             // Get instance of Vibrator from current Context
@@ -132,7 +172,9 @@ public class MainActivity extends AppCompatActivity {
                         */
 
                     } else {
-                        deviceString = name + " - RSSI " + rssi + "dBm" + distance_ft;
+                        //deviceString = name + " - RSSI " + rssi + "dBm" + distance_ft;
+                        deviceString = "Device:   " + address + "              " + "Distance: " + distance_round + "  Feet Away!!!";
+
 
                         /*
                         if (distance_ft < 6.00) {
